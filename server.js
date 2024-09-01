228ce797a74a78d5ab92c7d5837137c4ab0f1ead
loolishen <loolishen016@gmail.com>
2024-09-01 21:46:43 +0800

    uploaded server to handle the database and backend

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const path = require('path');
+
+const fileRoutes = require('./routers/fileroutes');
+
+const app = express();
+
+// Middleware
+app.use(cors({
+  origin: 'http://localhost:4200', 
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true
+}));
+
+app.use(bodyParser.json()); 
+app.use(express.json());
+
+// Serve static files from the uploads directory
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
+// Route definitions
+app.use('/api', fileRoutes);
+
+// MongoDB Connection
+mongoose.connect('mongodb://localhost:27017/filemanagement', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  console.log('Connected to MongoDB');
+}).catch(err => {
+  console.log('Failed to connect to MongoDB', err);
+});
+
+// Start the server
+const PORT_NUMBER = 8081;
+app.listen(PORT_NUMBER, () => {
+  console.log(`Listening on port ${PORT_NUMBER}`);
+});
